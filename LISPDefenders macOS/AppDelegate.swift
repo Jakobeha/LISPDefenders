981a1f312a9f25f82480cd26f68b3f948af29808
jakob <jakobeha@gmail.com>
2018-04-14 15:13:24 -0400

    Initial Commit

---
+++ LISPDefenders macOS/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  LISPDefenders macOS
+//
+//  Created by Jakob Hain on 4/14/18.
+//  Copyright © 2018 Jakobeha. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
